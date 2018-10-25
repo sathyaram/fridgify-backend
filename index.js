970dd7eae26318314341f1f2bf5e8bf3554ed7fa
Sathya Ram <sathyatheram@gmail.com>
2018-10-25 16:31:59 -0400

    first commit for backend from fridgify

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+// set up express app
+const app = express()
+
+// connect to mongoDB
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(process.env.MLAB_URL, {useNewUrlParser: true})
+} else {
+  mongoose.connect('mongodb://localhost/fridgify', {useNewUrlParser: true})
+}
+mongoose.Promise = global.Promise
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json())
+
+// initialize routes
+app.use('/', require('./routes/api'))
+
+// error handling middleware
+// app.use(function (err, req, res, next) {
+//   res.status(422).send({error: err.message})
+// })
+
+// listen for requests
+app.set('port', process.env.PORT || 3001)
+
+app.listen(app.get('port'), () => {
+  console.log(`PORT: ${app.get('port')}`)
+})
