74554df04df7e2d9b471c0ca8af5773279dcef5e
Sathya Ram <sathyatheram@gmail.com>
2018-10-26 11:55:19 -0400

    removed index code again and reordered categories

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 
 // set up express app
 const app = express()
-
-// connect to mongoDB
-if (process.env.NODE_ENV === 'production') {
-  mongoose.connect(process.env.MLAB_URL, {useNewUrlParser: true})
-} else {
-  mongoose.connect('mongodb://localhost/fridgify', {useNewUrlParser: true})
-}
-mongoose.Promise = global.Promise
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
